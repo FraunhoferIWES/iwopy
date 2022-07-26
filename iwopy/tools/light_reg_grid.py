94f0fe86334a1396be88023c52f4bb637992cebf
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2022-07-26 07:13:40 +0200

    removing subgrid option from light reg grid

--- iwopy/tools/light_reg_grid.py
+++ iwopy/tools/light_reg_grid.py
@@ -1,4 +1,3 @@
-from re import sub
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 
@@ -17,6 +16,8 @@ class LightRegGrid:
     n_steps : array-like
         The number of steps, len: n_dims. Use
         INT_INF for infinite.
+    **kwargs : dict, optional
+        Additional parameters for `RegularGridInterpolator`
 
     Attributes
     ----------
@@ -31,13 +32,35 @@ class LightRegGrid:
 
     INT_INF = -99999
 
-    def __init__(self, origin, deltas, n_steps):
+    def __init__(self, origin, deltas, n_steps, **kwargs):
 
         self.origin = np.array(origin, dtype=np.float64)
         self.n_steps = np.array(n_steps, dtype=np.int32)
         self.deltas = np.array(deltas, dtype=np.float64)
 
-        self._imem = {}
+        ocell = self.get_cell(self.origin) - self.origin[:, None]
+        self._interp = self._get_interp(ocell, **kwargs)
+
+    def _get_interp(self, cell0, **kwargs):
+        """
+        Helper function for interpolation object creation
+        """
+
+        n_dims = len(cell0)
+
+        cdata = np.zeros_like(cell0)
+        cdata[:, 1] = 1.0
+        cdata = np.meshgrid(*cdata, indexing="ij")
+
+        cshp = list(cdata[0].shape)
+        idata = np.zeros(cshp + [2**n_dims], dtype=np.float64)
+        isel = np.zeros_like(cdata[0], dtype=np.int32)
+        for di in range(n_dims):
+            dj = n_dims - 1 - di
+            isel[:] += (2**dj * cdata[di]).astype(np.int32)
+        np.put_along_axis(idata, isel[..., None], 1, axis=-1)
+
+        return RegularGridInterpolator(cell0, idata, **kwargs)
 
     @property
     def n_points(self):
@@ -68,90 +91,64 @@ class LightRegGrid:
         """
         return len(self.origin)
 
-    def get_corner(self, p, subgrid=None):
+    def get_corner(self, p):
         """
         Get the lower-left grid corner of a point.
 
         Parameters
         ----------
         p : numpy.ndarray
-            The point, shape: (n_sdims,)
-        subgrid : list of int, optional
-            The subgrid dimensions, shape: (n_sdims,)
-            or None for all
+            The point, shape: (n_dims,)
 
         Returns
         -------
         p0 : numpy.ndarray
-            The lower-left grid corner point, shape: (n_sdims,)
+            The lower-left grid corner point, shape: (n_dims,)
 
         """
-        if subgrid is None:
-            return self.origin + ((p - self.origin) // self.deltas) * self.deltas
-        else:
-            o = self.origin[subgrid]
-            d = self.deltas[subgrid]
-            if len(p) != len(subgrid):
-                p = p[subgrid]
-            return o + ((p - o) // d) * d
+        return self.origin + ((p - self.origin) // self.deltas) * self.deltas
 
-    def get_corners(self, pts, subgrid=None):
+    def get_corners(self, pts):
         """
         Get the lower-left grid corners of points.
 
         Parameters
         ----------
         pts : numpy.ndarray
-            The points space, shape: (n_pts, n_sdims)
-        subgrid : list of int, optional
-            The subgrid dimensions, shape: (n_sdims,)
-            or None for all
+            The points space, shape: (n_pts, n_dims)
 
         Returns
         -------
         p0 : numpy.ndarray
-            The lower-left grid corner points, shape: (n_pts, n_sdims)
+            The lower-left grid corner points, shape: (n_pts, n_dims)
 
         """
-        if subgrid is None:
-            o = self.origin[None, :]
-            d = self.deltas[None, :]
-        else:
-            o = self.origin[subgrid][None, :]
-            d = self.deltas[subgrid][None, :]
-            if pts.shape[1] != len(subgrid):
-                pts = pts[:, subgrid]
+        o = self.origin[None, :]
+        d = self.deltas[None, :]
         return o + ((pts - o) // d) * d
 
-    def get_cell(self, p, subgrid=None):
+    def get_cell(self, p):
         """
         Get the grid cell that contains a point.
 
         Parameters
         ----------
         p : numpy.ndarray
-            The point, shape: (n_sdims,)
-        subgrid : list of int, optional
-            The subgrid dimensions, shape: (n_sdims,)
-            or None for all
+            The point, shape: (n_dims,)
 
         Returns
         -------
         cell : numpy.ndarray
             The min and max values of each dimension. Shape:
-            (n_sdims, 2)
+            (n_dims, 2)
 
         """
-        n_dims = self.n_dims if subgrid is None else len(subgrid)
-        d = self.deltas if subgrid is None else self.deltas[subgrid]
-
-        cell = np.zeros((n_dims, 2), dtype=np.float64)
-        cell[:] = self.get_corner(p, subgrid)[:, None]
-        cell[:, 1] += d
-
+        cell = np.zeros((self.n_dims, 2), dtype=np.float64)
+        cell[:] = self.get_corner(p)[:, None]
+        cell[:, 1] += self.deltas
         return cell
 
-    def get_cells(self, pts, subgrid=None):
+    def get_cells(self, pts):
         """
         Get the grid cells that contain the given points,
         one cell per point.
@@ -159,60 +156,26 @@ class LightRegGrid:
         Parameters
         ----------
         pts : numpy.ndarray
-            The points, shape: (n_pts, n_sdims)
-        subgrid : list of int, optional
-            The subgrid dimensions, shape: (n_dims,)
-            or None for all
+            The points, shape: (n_pts, n_dims)
 
         Returns
         -------
         cells : numpy.ndarray
             The min and max values of each dimension. Shape:
-            (n_pts, n_sdims, 2)
+            (n_pts, n_dims, 2)
 
         """
         n_pts = pts.shape[0]
-        n_dims = self.n_dims if subgrid is None else len(subgrid)
-        d = self.deltas if subgrid is None else self.deltas[subgrid]
-
-        cells = np.zeros((n_pts, n_dims, 2), dtype=np.float64)
-        cells[:] = self.get_corners(pts, subgrid)[:, :, None]
-        cells[:, :, 1] += d[None, :]
-
+        cells = np.zeros((n_pts, self.n_dims, 2), dtype=np.float64)
+        cells[:] = self.get_corners(pts)[:, :, None]
+        cells[:, :, 1] += self.deltas[None, :]
         return cells
 
-    def _get_interp(self, cell0, subgrid, **kwargs):
-        """
-        Helper function for interpolation object lookup
-        """
-        mname = "all" if subgrid is None else tuple(subgrid)
-
-        if mname not in self._imem:
-
-            n_dims = len(cell0)
-
-            cdata = np.zeros_like(cell0)
-            cdata[:, 1] = 1.0
-            cdata = np.meshgrid(*cdata, indexing="ij")
-
-            cshp = list(cdata[0].shape)
-            idata = np.zeros(cshp + [2**n_dims], dtype=np.float64)
-            isel = np.zeros_like(cdata[0], dtype=np.int32)
-            for di in range(n_dims):
-                dj = n_dims - 1 - di
-                isel[:] += (2**dj * cdata[di]).astype(np.int32)
-            np.put_along_axis(idata, isel[..., None], 1, axis=-1)
-
-            self._imem[mname] = RegularGridInterpolator(cell0, idata, **kwargs)
-
-        return self._imem[mname]
-
-    def _error_info(self, p, subgrid):
+    def _error_info(self, p):
         """
         Helper for printing information at interpolation error
         """
-        n_dims = self.n_dims if subgrid is None else len(subgrid)
-        print("GDIM:", list(range(n_dims)) if subgrid is None else subgrid)
+        print("GDIM:", list(range(self.n_dims)))
         m = ", ".join(self.origin.astype(str).tolist())
         print("GMIN:", f"[{m}]")
         vmax = self.origin + self.n_steps * self.deltas
@@ -220,7 +183,7 @@ class LightRegGrid:
         print("GMAX:", f"[{m}]")
         print("P   :", p)
 
-    def interpolation_coeffs_point(self, p, subgrid=None, **kwargs):
+    def interpolation_coeffs_point(self, p):
         """
         Get the interpolation coefficients for
         a point.
@@ -236,34 +199,26 @@ class LightRegGrid:
         Parameters
         ----------
         p : numpy.ndarray
-            The point, shape: (n_sdims,)
-        subgrid : list of int, optional
-            The subgrid dimensions, shape: (n_sdims,)
-            or None for all
-        kwargs : dict, optional
-            Additional parameters for `RegularGridInterpolator`
+            The point, shape: (n_dims,)
 
         Returns
         -------
         gpts : numpy.ndarray
             The grid points relevant for interpolation,
-            shape: (n_gpts, n_sdims)
+            shape: (n_gpts, n_dims)
         coeffs : numpy.ndarray
             The interpolation coefficients, shape: (n_gpts,)
 
         """
-        cell = self.get_cell(p, subgrid)
+        cell = self.get_cell(p)
         p0 = cell[:, 0]
-        ocell = cell - p0[:, None]
         n_dims = len(p0)
         pts = p[None, :] - p0[None, :]
 
-        interp = self._get_interp(ocell, subgrid, **kwargs)
-
         try:
-            coeffs = interp(pts)[0]
+            coeffs = self._interp(pts)[0]
         except ValueError as e:
-            self._error_info(p, subgrid)
+            self._error_info(p)
             raise e
 
         gpts = np.stack(np.meshgrid(*cell, indexing="ij"), axis=-1)
@@ -276,12 +231,11 @@ class LightRegGrid:
 
         return gpts, coeffs
 
-    def _error_infos(self, pts, subgrid):
+    def _error_infos(self, pts):
         """
         Helper for printing information at interpolation error
         """
-        n_dims = self.n_dims if subgrid is None else len(subgrid)
-        print("GDIM:", list(range(n_dims)) if subgrid is None else subgrid)
+        print("GDIM:", list(range(self.n_dims)))
         m = ", ".join(self.origin.astype(str).tolist())
         print("GMIN:", f"[{m}]")
         vmax = self.origin + self.n_steps * self.deltas
@@ -302,7 +256,7 @@ class LightRegGrid:
                 f"Found {np.sum(sel)} points above higher bounds, e.g. point {s}: q = {pts[s]}"
             )
 
-    def interpolation_coeffs_points(self, pts, subgrid=None, **kwargs):
+    def interpolation_coeffs_points(self, pts):
         """
         Get the interpolation coefficients for a set of points.
 
@@ -317,41 +271,33 @@ class LightRegGrid:
         Parameters
         ----------
         pts : numpy.ndarray
-            The points, shape: (n_pts, n_sdims)
+            The points, shape: (n_pts, n_dims)
         subgrid : list of int, optional
-            The subgrid dimensions, shape: (n_sdims,)
+            The subgrid dimensions, shape: (n_dims,)
             or None for all
-        kwargs : dict, optional
-            Additional parameters for `RegularGridInterpolator`
 
         Returns
         -------
         gpts : numpy.ndarray
             The grid points relevant for interpolation,
-            shape: (n_pts, n_gpts, n_sdims)
+            shape: (n_pts, n_gpts, n_dims)
         coeffs : numpy.ndarray
             The interpolation coefficients, shape: (n_pts, n_gpts)
 
         """
-        if subgrid is not None and pts.shape[1] != len(subgrid):
-            pts = pts[:, subgrid]
-        n_dims = pts.shape[1]
-
-        cells = self.get_cells(pts, subgrid)
+        cells = self.get_cells(pts)
         ocell = cells[0] - cells[0, :, 0, None]
         p0 = cells[:, :, 0]
         opts = pts - p0
 
-        interp = self._get_interp(ocell, subgrid, **kwargs)
-
         try:
-            coeffs = interp(opts)
+            coeffs = self._interp(opts)
         except ValueError as e:
-            self._error_infos(opts, subgrid)
+            self._error_infos(opts)
             raise e
 
         ipts = np.stack(np.meshgrid(*ocell, indexing="ij"), axis=-1)
-        ipts = ipts.reshape(2**n_dims, n_dims)
+        ipts = ipts.reshape(2**self.n_dims, self.n_dims)
 
         sel = np.all(np.abs(coeffs) < 1.0e-14, axis=0)
         if np.any(sel):
