ac3daeea252e7f98b349f18d27d4e18c9d4d9fbf
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2022-07-28 15:11:57 +0200

    small dev tests

--- tests/test_discretize.py
+++ tests/test_discretize.py
@@ -2,6 +2,11 @@ import numpy as np
 
 import iwopy
 
+def f(x, y):
+    return x + np.sin(x - 0.6*y)
+
+def g(x, y):
+    return [1 + np.cos(x - 0.6*y), -0.6*np.cos(x - 0.6*y)]
 
 class Obj1(iwopy.Objective):
     
@@ -13,20 +18,15 @@ class Obj1(iwopy.Objective):
 
     def calc_individual(self, vars_int, vars_float, problem_results):
         x, y = vars_float
-        return [x + np.sin(x - 0.6*y)]
+        return [f(x, y)]
 
     def calc_population(self, vars_int, vars_float, problem_results):
         x, y = vars_float[:, 0], vars_float[:, 1]
-
-        def f(x, y):
-            return np.sin(x + 0.6*y)
-
         return f(x, y)[:, None]
 
     def ana_grad(self, pvars0_float):
         x, y = pvars0_float
-        return np.array([1 + np.cos(x - 0.6*y), -0.6*np.cos(x - 0.6*y)])
-
+        return np.array(g(x, y))
 
 def test_grad():
 
@@ -43,7 +43,7 @@ def test_grad():
 
         (2, 2, True, 0.01, 0.02, 5e-5),
         (2, 2, True, 0.001, 0.002, 5e-7),
-        #(2, 2, True, 0.0001, 0.0002, 5e-9), NEEDS CHECK, MISMATCH?
+        #(2, 2, True, 0.0001, 0.0002, 5e-9), # NEEDS CHECK, MISMATCH?
     )
     N = 50
 
@@ -57,8 +57,8 @@ def test_grad():
             min_float_vars={"x": 1., "y": 0.},
             max_float_vars={"x": 2., "y": 3.},
         )
-        f = Obj1(p, "f")
-        p.add_objective(f, varmap_float={"x": "x", "y": "y"})
+        obj1 = Obj1(p, "f")
+        p.add_objective(obj1, varmap_float={"x": "x", "y": "y"})
 
         gp = iwopy.DiscretizeRegGrid(p, {"x": dx, "y": dy}, fd_order={"x": ox, "y": oy})
         gp.initialize(verbosity=1)
@@ -70,7 +70,7 @@ def test_grad():
             g = gp.get_gradients(vars_int=[], vars_float=p0, pop=pop)[0]
             print("g =", g)
 
-            a = f.ana_grad(p0)
+            a = obj1.ana_grad(p0)
             print("a =", a)
 
             d = np.abs(a - g)
