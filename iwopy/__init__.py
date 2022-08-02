66fa4ba35d14823aee55fdfa97ef81c5097629f6
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2022-08-02 12:03:32 +0200

    dev inits

--- iwopy/__init__.py
+++ iwopy/__init__.py
@@ -2,6 +2,9 @@ from .core import Problem, Objective, Constraint, Memory
 from .problems import SimpleProblem
 from .wrappers import ProblemWrapper, DiscretizeRegGrid
 
+from . import core
+from . import problems
+from . import wrappers
 from . import utils
 from . import interfaces
 from . import benchmarks
