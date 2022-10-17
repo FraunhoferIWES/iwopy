36a58a1f2ab1ea4825f2f58333deb981dc14159f
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2022-10-17 12:43:29 +0200

    extending examples notebook

--- examples/electrostatics/run_gg.py
+++ examples/electrostatics/run_gg.py
@@ -2,7 +2,7 @@ import numpy as np
 import argparse
 import matplotlib.pyplot as plt
 
-from iwopy import DiscretizeRegGrid, LocalFD
+from iwopy import LocalFD
 from iwopy.optimizers import GG
 from model import ChargesProblem
 
@@ -33,14 +33,7 @@ if __name__ == "__main__":
     plt.show()
     plt.close(fig)
 
-    """
-    gproblem = DiscretizeRegGrid(
-        problem, deltas=1e-8, fd_order=args.order, fd_bounds_order=1, tol=1e-10, interpolation=args.interpolation
-    )
-    """
-    gproblem = LocalFD(
-        problem, deltas=1e-2, fd_order=args.order, fd_bounds_order=1
-    )
+    gproblem = LocalFD(problem, deltas=1e-2, fd_order=args.order)
     gproblem.initialize()
 
     solver = GG(
